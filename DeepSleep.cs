d463d83de906e68589c55ef90f242cc7b67bf17b
Frederico <fredericolarangeira@hotmail.com>
2022-09-16 22:14:07 -0300

    changed names

--- DeepSleep.cs
+++ DeepSleep.cs
@@ -3,39 +3,12 @@ using XRL;
 using XRL.World;
 using XRL.World.Parts;
 
-/*
-[Serializable]
-public class HealEveryTurn : IActivePart
-{
-
-    public override bool WantEvent(int ID, int cascade)
-    {
-        return
-            base.WantEvent(ID, cascade)
-            || ID == EndTurnEvent.ID
-        ;
-    }
-
-    public override bool HandleEvent(EndTurnEvent E)
-    {
-        if (IsReady(UseCharge: true))
-        {
-            foreach (GameObject obj in GetActivePartSubjects())
-            {
-                obj.Heal(1);
-            }
-        }
-        return true;
-    }
-
-}*/
-
 
 [Serializable]
-public class HealSelfEveryTurn : IActivePart
+public class SleepDeprivation : IActivePart
 {
 
-    public HealSelfEveryTurn()
+    public SleepDeprivation()
     {
         WorksOnSelf = true;
     }
@@ -67,13 +40,13 @@ public class HealSelfEveryTurn : IActivePart
 }
 
 [PlayerMutator]
-public class MyPlayerMutator : IPlayerMutator
+public class PartAdder : IPlayerMutator
 {
     public void mutate(GameObject player)
     {
         // modify the player object when a New Game begins
         // for example, add a custom part to the player:
-        player.AddPart<HealSelfEveryTurn>();
+        player.AddPart<SleepDeprivation>();
     }
 }
 
